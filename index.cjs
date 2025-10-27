b893142fa18a5671fa67f01a9e327447f1a6e708
Eugene July <62775429+watsmanameha@users.noreply.github.com>
2025-10-27 15:19:36 +0300

    week 5 lms

--- index.cjs
+++ index.cjs
@@ -1,11 +1,7 @@
 const express = require('express');
 const multer = require('multer');
-const sharp = require('sharp');
-const https = require('https');
-const fs = require('fs');
 
 const app = express();
-const upload = multer(); // сохраняем в оперативной памяти
 
 const LOGIN = "edzhulaj";
 
@@ -13,24 +9,15 @@ app.get('/login', (req, res) => {
     res.type('text/plain').send(LOGIN);
 });
 
-app.post("/size2json", upload.single("image"), async (req, res) => {
-    try {
-        if (!req.file) {
-            return res.status(400).json({ error: "Не передано поле image" });
-        }
-
-        const metadata = await sharp(req.file.buffer).metadata();
-
-        res.json({
-            width: metadata.width,
-            height: metadata.height
-        });
-    } catch (err) {
-        res.status(500).json({ error: "Ошибка обработки изображения" });
-    }
+app.get('/hour', (req, res) => {
+  const now = new Date();
+  const utcHour = now.getUTCHours();
+  const moscowHour = (utcHour + 3) % 24; // Москва = UTC+3
+  const hh = String(moscowHour).padStart(2, '0');
+  res.type('text/plain').send(hh);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Сервер запущен на порту ${PORT}`);
+  console.log(`HTTP server running on http://localhost:${PORT}`);
 });
